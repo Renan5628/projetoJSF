28e55ebbe8a6817a5a7920b0dc956000684f483a
Gabriel <Gabriel@DESKTOP-M6GURVS>
2023-11-10 02:18:56 -0300

    Exercício com backing bean.

--- projetoJSF/src/main/java/br/com/projetoJSF/PessoaBean.java
+++ projetoJSF/src/main/java/br/com/projetoJSF/PessoaBean.java
@@ -5,13 +5,14 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
 
 @ManagedBean(name = "pessoaBean")
 @ViewScoped
 public class PessoaBean {
 	
 	private String nome;
-	
+	private HtmlCommandButton commandButton;
 	private List<String>nomes = new ArrayList<String>();
 	
 	
@@ -27,11 +28,21 @@ public class PessoaBean {
 	public void setNomes(List<String> nomes) {
 		this.nomes = nomes;
 	}
-	
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
 	
 	
 	public String addNome() {
 		nomes.add(nome);
+		
+		if(nomes.size() > 3) {
+			commandButton.setDisabled(true);
+	}
+		
 		return "";
 	}
 	
